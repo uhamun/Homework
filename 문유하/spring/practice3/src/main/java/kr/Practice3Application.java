8d8a156c5a2ed74d46b223fd62ccfca50c66cb62
uhamun <uhamun@gmail.com>
2022-07-21 15:47:34 +0900

    과제2

---
+++ 문유하/spring/practice3/src/main/java/kr/Practice3Application.java
@@ -0,0 +1,13 @@
+package kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice3Application.class, args);
+    }
+
+}
